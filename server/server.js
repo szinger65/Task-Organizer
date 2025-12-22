8267d0b0295573f1ba4ea76a97bffab19b9ad386
Solaiman <solaimanzinger@icloud.com>
2025-12-22 16:08:57 +0100

    add root package.json for the react front end

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config({ path: './mongo.env' });
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
